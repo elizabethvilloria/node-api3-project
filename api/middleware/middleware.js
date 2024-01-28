97e4d049b86e3973904642067e37675538958770
elizabethvilloria <lizvilloria@gmail.com>
2024-01-28 18:44:47 -0500

    finished mvp

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -13,7 +13,7 @@ async function validateUserId(req, res, next) {
     const user = await User.getById(req.params.id)
     if (!user) {
       res.status(404).json({
-        message: 'no such user'
+        message: 'user not found'
       })
     } else {
       req.user = user
@@ -30,7 +30,7 @@ function validateUser(req, res, next) {
   const { name } = req.body
   if (!name || !name.trim) {
     res.status(400).json({
-      message: "missing required text field"
+      message: "missing required name"
     })
   } else {
     req.name = name.trim()
